6b22d54c47f70079898f3ca1e1f18e05c6729a55
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-01-26 17:08:16 +0100

    Collections Review

--- collections-review/src/Student.java
+++ collections-review/src/Student.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Student {
+import java.util.Objects;
+
+public class Student {
+    int id;
+    String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
