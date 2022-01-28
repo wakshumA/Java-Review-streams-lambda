e7a5feaff3d5908ed1ef61f674dc49331f776b08
cybertekschool <github@cybertekschool.com>
2022-01-27 21:47:35 -0500

    Lombok

--- lambda/src/main/java/ArraySorting.java
+++ lambda/src/main/java/ArraySorting.java
@@ -12,10 +12,26 @@ public class ArraySorting {
 
 
 
+       Sorting quickSort = () -> System.out.println("Quick sorting");
+       as.sort(quickSort);
+
+       Sorting bubleSort = () -> System.out.println("Buble sorting");
+       as.sort(bubleSort);
+
+
+       as.sort(() -> System.out.println("Buble sorting"));
+
+
+
+
+
 
     }
 
     private void sort(Sorting sorting) {
+
         sorting.sort();
     }
 }
+
+
--- lambda/src/main/java/Sorting.java
+++ lambda/src/main/java/Sorting.java
@@ -1,3 +1,6 @@
+
+@FunctionalInterface
 public interface Sorting {
     public void sort();
+
 }
