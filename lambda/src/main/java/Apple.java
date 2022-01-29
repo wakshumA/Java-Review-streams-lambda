c873f8d13b3bd26daf8bb92baddabb18f46cc74a
cybertekschool <github@cybertekschool.com>
2022-01-29 14:01:47 -0500

    Functional Interface

--- lambda/src/main/java/Apple.java
+++ lambda/src/main/java/Apple.java
@@ -1,3 +1,4 @@
+
 public class Apple {
 
     private int weight;
--- lambda/src/main/java/AppleTest.java
+++ lambda/src/main/java/AppleTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AppleTest {
 
@@ -13,17 +14,34 @@ public class AppleTest {
 
 
 
-        List<Apple> heavyApple = filterApples(inventory,new AppleHeavyPredicate());
-        System.out.println(heavyApple);
+//        List<Apple> heavyApple = filterApples(inventory,new AppleHeavyPredicate());
+//        System.out.println(heavyApple);
+//
+//        List<Apple> greenApple = filterApples(inventory,new AppleGreenColorPredicate());
+//        System.out.println(greenApple);
 
-        List<Apple> greenApple = filterApples(inventory,new AppleGreenColorPredicate());
+
+        List<Apple> greenApple = filterApples(inventory, apple -> apple.getColor().equals(Color.GREEN) );
         System.out.println(greenApple);
 
 
 
     }
 
-    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
+//    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
+//
+//        List<Apple> result = new ArrayList<>();
+//
+//        for(Apple apple : inventory){
+//            if(applePredicate.test(apple)){
+//                result.add(apple);
+//            }
+//        }
+//
+//        return result;
+//    }
+
+    private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> applePredicate) {
 
         List<Apple> result = new ArrayList<>();
 
